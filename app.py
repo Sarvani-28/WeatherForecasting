abe732ed500dd67bdb0e303d8bf1cfc35ef781b7
Sarvani Mulukutla <sarvanimulukutla2804@gmail.com>
2025-07-17 18:58:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "f5453e6a1188e9033aba4b68ad798eab"  # Replace with your OpenWeatherMap API key
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city']
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={f5453e6a1188e9033aba4b68ad798eab}&units=metric"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'humidity': data['main']['humidity']
+            }
+        else:
+            weather_data = {'error': 'City not found. Please enter a valid city.'}
+
+    return render_template('index.html', weather=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
